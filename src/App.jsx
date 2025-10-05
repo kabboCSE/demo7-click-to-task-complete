48c55453c3bb6b188f6d54925a4a8edddbd9150a
kabboCSE <shahriar15-10494@diu.edu.bd>
2025-10-05 18:06:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div>
+      <Nav></Nav>
+      <h2>Main Section</h2>
+      <footer>footer</footer>
+    </div>
+  );
+}
+
+export default App;
